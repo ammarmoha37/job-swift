13ab0d59fd85835775821d82abb87dbeee57092b
Ammar <101853260+ammarmoha37@users.noreply.github.com>
2024-06-24 06:11:24 +0300

    feat: Implement data fetching from Firebase for dynamic data rendering in candidates component

--- src/app/components/candidates/candidates.component.ts
+++ src/app/components/candidates/candidates.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable, map } from 'rxjs';
+import { Candidate } from '../../models/candidate.model';
+import { CompanyService } from '../../services/company.service';
+import { CandidateService } from '../../services/candidate.service';
 
 @Component({
   selector: 'app-candidates',
   templateUrl: './candidates.component.html',
   styleUrl: './candidates.component.css'
 })
-export class CandidatesComponent {
+export class CandidatesComponent implements OnInit {
+  candidates$: Observable<Candidate[]>;
 
+  constructor(private candidateService: CandidateService) { }
+
+  ngOnInit(): void {
+    this.candidates$ = this.candidateService.getAllCandidates().pipe(
+      map(actions => {
+        return actions.map(a => {
+          const data = a.payload.doc.data() as Candidate;
+          const id = a.payload.doc.id;
+          return { id, ...data };
+        });
+      })
+    );
+  }
 }
